74f5d9e129b3462a14cd78139e536edccd4702e3
RayhungKao <ryamkao2339@gmail.com>
2022-02-08 15:53:03 +0800

    contract deployment by infura

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const mnemonic = "kitchen jump garage nation quick primary siege filter trash brain affair please";
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -12,6 +14,12 @@ module.exports = {
   networks: {
     develop: {
       port: 8545,
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/af03294c72c44ac9b2d4638cf9b92769")
+      },
+      network_id: 3
     }
   }
 };
