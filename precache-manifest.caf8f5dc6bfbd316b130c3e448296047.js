fbddd004afd6cd41d3853c98a675cb947f67ec45
RayhungKao <ryamkao2339@gmail.com>
2022-02-08 15:21:51 +0800

    Updates

---
+++ precache-manifest.caf8f5dc6bfbd316b130c3e448296047.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "729097ee7e11437fb99829f3c94a1b20",
+    "url": "/transaction-dapp/index.html"
+  },
+  {
+    "revision": "be2d48f2635fd8326c28",
+    "url": "/transaction-dapp/static/css/2.b6834657.chunk.css"
+  },
+  {
+    "revision": "58c5423d5f5f8738e25e",
+    "url": "/transaction-dapp/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "be2d48f2635fd8326c28",
+    "url": "/transaction-dapp/static/js/2.840d9df7.chunk.js"
+  },
+  {
+    "revision": "58c5423d5f5f8738e25e",
+    "url": "/transaction-dapp/static/js/main.df190011.chunk.js"
+  },
+  {
+    "revision": "50397d572c7bba1b1221",
+    "url": "/transaction-dapp/static/js/runtime-main.af2af5f6.js"
+  }
+]);
